4ff0e1fd0ab15ef3fa7bfb41c3579b12457736aa
Johannes Bornhold <johannes@bornhold.name>
2013-01-14 07:11:22 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import sys, os
+
+version_filename = os.path.join(os.path.dirname(__file__), 'VERSION')
+version = open(version_filename).read().strip()
+
+setup(
+    name = 'int_todo',
+    version = version,
+    author = 'Johannes Bornhold',
+    author_email = 'johannes@bornhold.name',
+    description = 'Example application to work with todo items',
+    url = '',
+    install_requires = [
+        'distribute',
+        'django',
+        'django-piston',
+        'south',
+        ],
+    package_dir = {'': '.'},
+    packages = find_packages('.'),
+    include_package_data = True,
+    package_data = {
+        },
+    namespace_packages = [
+        ],
+    entry_points = {
+        },
+    zip_safe = False,
+    )
